cdb8c79eef41d58b2202f565ee0da17670f2de8e
Mylun <mylunjackson87@gmail.com>
2026-03-02 09:17:17 -0800

    Initial Commit

---
+++ MyBanBoard/MyBanBoardApp.swift
@@ -0,0 +1,32 @@
+//
+//  MyBanBoardApp.swift
+//  MyBanBoard
+//
+//  Created by Mylun Jackson on 3/2/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MyBanBoardApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
